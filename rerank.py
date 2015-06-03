307ae8c728276a00644ed01c793fe484cde1a0c4
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-03 02:55:01 -0700

    added celery task

--- rerank.py
+++ rerank.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python
 import os, time
-
+from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unconnectedreddit.settings")
 from links.models import Link
 
+app = Celery('rerank', broker='amqp://guest@localhost//')
+
+@app.task
 def rank_all():
     for link in Link.with_votes.all():
         link.set_rank()
-
-def show_all():
-    print "\n".join("%10s %0.2f" % (l.description, l.rank_score,
-                         ) for l in Link.with_votes.all())
-    print "----\n\n\n"
-
-if __name__=="__main__":
-    while 1:
-        print "---"
-        rank_all()
-        show_all()
-        time.sleep(5)
