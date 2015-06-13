2daf3197d91551d78e5b750f1aed924b75e043cf
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-12 19:46:05 -0700

    DEBUG = false TEMPLATE_DEBUG = false

--- unconnectedreddit/settings.py
+++ unconnectedreddit/settings.py
@@ -10,8 +10,8 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 #heroku config:set ON_HEROKU=1 
 #heroku ps:scale web=1 to put in a dyno
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = False ################# change this in live environment
+TEMPLATE_DEBUG = False
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
