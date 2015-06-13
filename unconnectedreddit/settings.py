4425fd707c73ae589d8bdeab812f7e012c54211a
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-13 04:16:35 -0700

    solving csrf issues

--- unconnectedreddit/settings.py
+++ unconnectedreddit/settings.py
@@ -203,8 +203,9 @@ else:
     }
 }
 
+SECURE_SSL_REDIRECT = False
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 import djcelery
