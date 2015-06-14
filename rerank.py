b54445256e6b7dab6b592e41f40568a926352c83
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-14 03:05:04 -0700

    small tweaks

--- rerank.py
+++ rerank.py
@@ -8,14 +8,6 @@ def rank_all():
     for link in Link.with_votes.all():
         link.set_rank()
 
-import time
-
-def show_all():
-    print "\n".join("%10s %0.2f" % (l.submitter, l.rank_score,
-                         ) for l in Link.with_votes.all())
-    print "----\n\n\n"
-
 if __name__=="__main__":
-        print "---"
         rank_all()
-        show_all()
+        
