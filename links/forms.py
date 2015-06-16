98031563b2fb3a24f66660ff16db6efcada8b78f
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-15 17:46:27 -0700

    adding radio buttons to UserProfileForm

--- links/forms.py
+++ links/forms.py
@@ -3,9 +3,20 @@ from .models import UserProfile, Link, Vote
 from django.contrib.auth.models import User
 
 class UserProfileForm(forms.ModelForm):
+    MardAurat = (
+    	(1,'Aurat'),
+    	(0,'Mard'),
+    	)
+    MaritalStatus = (
+    	(1,'Yes'),
+    	(0,'No'),
+    	)
+    gender = forms.TypedChoiceField(choices=MardAurat, widget=forms.RadioSelect, coerce=int)
+    shadi_shuda = forms.TypedChoiceField(choices=MaritalStatus, widget=forms.RadioSelect, coerce=int)
     class Meta:
         model = UserProfile
-        exclude = ("user") #so user doesn't show, but the extended attributes of bio and mobile number do show
+        exclude = ("user") #so user doesn't show, but the extended attributes of bio and mobile number do show    
+    
 
 class LinkForm(forms.ModelForm):
     class Meta:
