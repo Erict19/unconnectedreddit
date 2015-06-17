5b3fa88bdfa48a07ef4e832ec73db7eb8f8b589c
Hassan Baig <hassan.baig@alumni.duke.edu>
2015-06-16 22:58:11 -0700

    Middleware changes to detect small screens

--- unconnectedreddit/settings.py
+++ unconnectedreddit/settings.py
@@ -99,9 +99,12 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'unconnectedreddit.mobi.middleware.MobileDetectionMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+MOBI_DETECT_TABLET = True
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
